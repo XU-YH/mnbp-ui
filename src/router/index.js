6a4c42b620f0bf905afcb7946d21e81b9e21a1b8
XU-YH <914450250@qq.com>
2020-01-03 16:58:02 +0800

    修改前后端访问根路径和打包子目录

--- src/router/index.js
+++ src/router/index.js
@@ -132,6 +132,7 @@ export const constantRoutes = [
 
 export default new Router({
   mode: 'history', // 去掉url中的#
+  base: '/mnbp/',
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
